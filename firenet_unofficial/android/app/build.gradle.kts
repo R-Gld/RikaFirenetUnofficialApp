156f974f4d74f8dabcbab2c8f28597f25aec938c
Romain Galland <romain.galland@infomaniak.com>
2025-12-30 01:21:27 +0100

    Config: Personnaliser le package name et le nom de l'application
    Changement du package par défaut vers un identifiant personnalisé :
    
    Modifications Android :
    - Package name : com.example.firenet_unofficial → fr.Rgld_.firenet_unofficial
    - Application ID : com.example.firenet_unofficial → fr.Rgld_.firenet_unofficial
    - Label app : firenet_unofficial → Rika Firenet
    - MainActivity.kt : mise à jour du package
    
    Modifications Linux :
    - Application ID : com.example.firenet_unofficial → fr.Rgld_.firenet_unofficial
    
    Le nom de l'application affiche maintenant "Rika Firenet" sur
    l'écran d'accueil et dans la liste des applications.

--- firenet_unofficial/android/app/build.gradle.kts
+++ firenet_unofficial/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.firenet_unofficial"
+    namespace = "fr.Rgld_.firenet_unofficial"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.firenet_unofficial"
+        applicationId = "fr.Rgld_.firenet_unofficial"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
