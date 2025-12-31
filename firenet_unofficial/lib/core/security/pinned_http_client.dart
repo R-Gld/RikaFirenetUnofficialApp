431148a32b6c0683b65a60b9cacae648919403bf
Romain Galland <romain.galland@infomaniak.com>
2025-12-31 16:58:04 +0100

    fix: simplify PinnedHttpClientAdapter to extend IOHttpClientAdapter properly

--- firenet_unofficial/lib/core/security/pinned_http_client.dart
+++ firenet_unofficial/lib/core/security/pinned_http_client.dart
@@ -1,41 +1,18 @@
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:dio/io.dart';
-import 'package:dio/dio.dart';
 import 'certificate_pinning.dart';
 
 /// HTTP client adapter with certificate pinning
 ///
 /// Validates SSL certificates against pinned public key hashes
-class PinnedHttpClientAdapter implements HttpClientAdapter {
-  final IOHttpClientAdapter _adapter = IOHttpClientAdapter();
-
-  @override
-  void close({bool force = false}) {
-    _adapter.close(force: force);
-  }
-
-  @override
-  Future<ResponseBody> fetch(
-    RequestOptions options,
-    Stream<Uint8List>? requestStream,
-    Future<void>? cancelFuture,
-  ) async {
-    // Configure the HTTP client with certificate pinning
-    final httpClient = HttpClient();
-    httpClient.badCertificateCallback = (X509Certificate cert, String host, int port) {
-      return CertificatePinningValidator.validateCertificate(cert, host, port);
-    };
-
-    // Create a new adapter with our configured client
-    final adapter = IOHttpClientAdapter(
-      createHttpClient: () => httpClient,
-    );
-
-    try {
-      return await adapter.fetch(options, requestStream, cancelFuture);
-    } finally {
-      adapter.close(force: true);
-    }
-  }
+class PinnedHttpClientAdapter extends IOHttpClientAdapter {
+  PinnedHttpClientAdapter() : super(
+    createHttpClient: () {
+      final client = HttpClient();
+      client.badCertificateCallback = (X509Certificate cert, String host, int port) {
+        return CertificatePinningValidator.validateCertificate(cert, host, port);
+      };
+      return client;
+    },
+  );
 }
